f4c1de2d4cef7e6f52f6193611b81161f55f2fc9
Alittfre <alittfre@gmail.com>
2024-03-13 21:19:45 +0800

    add nav

---
+++ src/views/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+<appTopnav/>
+<appHeader/>
+    <nav>顶部通栏</nav>
+    <header>头部</header>
+    <main>
+        <!-- 二级路由 -->
+        <router-view></router-view>
+    </main>
+    <footer>底部</footer>
+</template>
+
+<script>
+import appTopnav from '@/components/app-topnav.vue'
+import appHeader from '@/components/app-header.vue'
+export default {
+  name: 'xtx-layout',
+  components: {
+    appTopnav,
+    appHeader
+  }
+}
+</script>
+<style scoped lang='less'></style>
