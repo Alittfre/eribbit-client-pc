f3adf429a547642b9c14cae13838453b22f35f55
Alittfre <alittfre@gmail.com>
2024-03-14 17:56:54 +0800

    add footer

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,23 +1,23 @@
 <template>
 <appTopnav/>
 <appHeader/>
-    <nav>顶部通栏</nav>
-    <header>头部</header>
-    <main>
+    <main class="app-body">
         <!-- 二级路由 -->
         <router-view></router-view>
     </main>
-    <footer>底部</footer>
+    <appFooter/>
 </template>
 
 <script>
 import appTopnav from '@/components/app-topnav.vue'
 import appHeader from '@/components/app-header.vue'
+import appFooter from '@/components/app-footer.vue'
 export default {
   name: 'xtx-layout',
   components: {
     appTopnav,
-    appHeader
+    appHeader,
+    appFooter
   }
 }
 </script>
